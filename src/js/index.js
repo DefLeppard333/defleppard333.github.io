50d5f4e21da9da448b13d521b46666c3735966c5
Igor Shpykuliak <defleppard333@gmail.com>
2022-05-12 01:10:39 +0300

    feat: side section is done

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-import createBurger from "burger.js"
+// Progress bar
+let progress = document.querySelectorAll('.main-aside__skills-progress');
+let progressNumber = document.querySelectorAll('.main-aside__skills-progress-num');
+
+(function move() {
+    progress.forEach((elem,i) =>{
+        let start = 0;
+        let interval = setInterval(()=>{
+            elem.value = start;
+            let number = elem.dataset.current;
+            progressNumber[i].innerHTML = start + '%';
+            start++;
+            if(start > number){
+                clearInterval(interval);
+            }
+
+        }, 10);
+    });
+})();
+
